d80762045dae088893c22ec3c5ce95b3c023188a
ulya-kham <ulya.xamzina@inbox.ru>
2026-02-18 09:13:13 +0500

    🟥 Delete Laba8_Git

--- Laba8_Git/homedyn1.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <vector>
-#include <string>
-
-bool read_board(const std::string& fname, std::vector<std::vector<int>>& board, int& n);
-int find_max_sum(const std::vector<std::vector<int>>& board, int n);
-bool write_result(const std::string& fname, int result);
