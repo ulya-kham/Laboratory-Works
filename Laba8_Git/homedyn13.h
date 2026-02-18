667af76511e9f5e74f86635076e26decbefc4840
ulya-kham <ulya.xamzina@inbox.ru>
2026-02-18 09:14:46 +0500

    🟥 Delete Laba8_Git

--- Laba8_Git/homedyn13.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <string>
-
-bool read_params(const std::string& fname, int& k, int& n);
-double count_valid_numbers(int k, int n);
-bool write_result(const std::string& fname, double result); 
